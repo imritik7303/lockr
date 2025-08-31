124340e230b47ff1c7272f2f36f9485fd32616e4
imritik7303 <imritik7303@gmail.com>
2025-09-01 03:40:40 +0530

    added profile page and logout functionality

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,26 @@
+import { SignOutButton } from '@/components/sign-out-button'
+import { auth } from '@/lib/auth'
+
+import { headers } from 'next/headers'
+import React from 'react'
+
+export default async function Profile() {
+    const session = await auth.api.getSession({
+        headers:await headers()
+    })
+  if(!session) {
+    return <p className='text-destructive'>Unauthorized</p>
+  }  
+  return (
+    <div className="px-8 py-16 container
+        mx-auto max-w-screen-lg space-y-8">
+            <div className="space-y-8">
+                <h1 className="text-3xl font-bold">Profile</h1>
+            </div>
+            <SignOutButton/>
+            <pre className='text-sm overflow-clip'>
+                {JSON.stringify(session ,null ,2)}
+            </pre>
+    </div>
+  )
+}
---
+++ src/components/sign-out-button.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/router";
+import { signOut } from "@/lib/auth-client";
+import { toast } from "sonner";
+
+export const SignOutButton = () => {
+  const [isPending, setIsPending] = useState(false);
+  const router = useRouter();
+
+  async function handleClick() {
+    await signOut({
+      fetchOptions: {
+        onError: (ctx) => {
+          toast.error(ctx.error.message);
+        },
+        onSuccess: () => {
+          toast.success("You’ve logged out. See you soon!");
+          router.push("/auth/login");
+        },
+      },
+    });
+  }
+
+  return (
+    <Button
+      onClick={handleClick}
+      size="sm"
+      variant="destructive"
+      disabled={isPending}
+    >
+      Sign out
+    </Button>
+  );
+};
