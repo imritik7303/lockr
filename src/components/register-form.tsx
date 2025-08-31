829ff0f9eb4b91a317bcb7e792b29e125cdabe04
imritik7303 <imritik7303@gmail.com>
2025-09-01 02:56:04 +0530

    register form created and setup

---
+++ src/components/register-form.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { signUp } from "@/lib/auth-client";
+
+
+export const RegisterForm = () => {
+    async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+        evt.preventDefault();
+         const formData =new FormData(evt.target as HTMLFormElement)
+
+         const name = String(formData.get("name"))
+         if(!name) return toast.error("please enter your name")
+         
+         const email = String(formData.get("email"))
+         if(!name) return toast.error("please enter your email")
+         
+        const password = String(formData.get("password"))
+         if(!name) return toast.error("please enter your password")    
+       
+         await signUp.email(
+            {
+                name,
+                email,
+                password
+            },
+            {
+                onRequest:() => {},
+                onResponse:() => {},
+                onError:(ctx) => {
+                    toast.error(ctx.error.message)
+                },
+                onSuccess:()=>{},
+            }
+         )   
+    }
+
+
+
+    return (
+        <form onSubmit={handleSubmit} className="max-w-sm w-full space-y-4">
+            <div className="space-y-2">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" name="name" />
+            </div>
+
+            <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" id="email" name="email" />
+            </div>
+
+            <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" name="password" />
+            </div>
+
+            <Button type="submit" className="w-full" >
+                Register
+            </Button>
+        </form>
+    );
+}
